7f9e6fd20dc7dd6ec7c9ca83f0449a59b927b999
Josué Camelo dos Santos Ferreira <josueprg@gmail.com>
2021-12-15 15:36:39 -0300

    Toogle Camera

--- App.js
+++ App.js
@@ -1,19 +1,40 @@
-import React, { useState } from 'react';
-import {View, Text, StatusBar, StyleSheet, TouchableOpacity, Modal,} from 'react-native';
-import { RNCamera } from 'react-native-camera';
+import React, {useState} from 'react';
+import {
+  View,
+  Text,
+  StatusBar,
+  StyleSheet,
+  TouchableOpacity,
+  Modal,
+  Image,
+} from 'react-native';
+import {RNCamera} from 'react-native-camera';
 
-export default function App(){
+export default function App() {
   const [type, setType] = useState(RNCamera.Constants.Type.back);
   const [open, setOpen] = useState(false);
+  const [capturedPhoto, setCapturedPhoto] = useState(null);
 
-  function takePicture(){
+  async function takePicture(camera) {
+    const options = {
+      quality: 0.5,
+      base64: true,
+    };
+
+    const data = await camera.takePictureAsync(options);
+    setCapturedPhoto(data.uri);
     setOpen(true);
+    console.log('Foto tirada: ' + data.uri);
   }
 
-  return(
+  function toogleCamera(){
+    setType(type === RNCamera.Constants.Type.back ? RNCamera.Constants.Type.front : RNCamera.Constants.Type.back)
+  }
+
+  return (
     <View style={styles.container}>
       <StatusBar hidden={true} />
-      
+
       <RNCamera
         style={styles.preview}
         type={type}
@@ -22,60 +43,74 @@ export default function App(){
           title: 'Permissao para usar a camera',
           message: 'Nós precisamos usar a sua camera',
           buttonPositive: 'Ok',
-          buttonNegative: 'Cancelar'
-        }}
-      >
-        { ({ camera, status, recordAndroidPermissionStatus }) => {
-          if(status !== 'READY') return <View/>;
-          return(
-            <View 
-            style={{marginBottom: 35, flexDirection: 'row', alignItems: 'flex-end', justifyContent: 'space-between'}}
-            >
+          buttonNegative: 'Cancelar',
+        }}>
+        {({camera, status, recordAndroidPermissionStatus}) => {
+          if (status !== 'READY') return <View />;
+          return (
+            <View
+              style={{
+                marginBottom: 35,
+                flexDirection: 'row',
+                alignItems: 'flex-end',
+                justifyContent: 'space-between',
+              }}>
               <TouchableOpacity
-                onPress={()=> takePicture() }
-                style={styles.capture}
-              >
+                onPress={() => takePicture(camera)}
+                style={styles.capture}>
                 <Text>Tirar foto</Text>
               </TouchableOpacity>
-              <TouchableOpacity
-                onPress={()=> {}}
-                style={styles.capture}
-              >
+              <TouchableOpacity onPress={() => {}} style={styles.capture}>
                 <Text>Album</Text>
               </TouchableOpacity>
             </View>
           );
         }}
       </RNCamera>
+      
+      <View style={styles.camPosition}>
+        <TouchableOpacity onPress={toogleCamera}>
+          <Text>Inverter Camera</Text>
+        </TouchableOpacity>
+      </View>
 
-
-      <Modal animationType="slide" transparent={false} visible={open}>
-        <View style={{flex:1, justifyContent: 'center', alignItems: 'center', margin: 20}} >
-          <TouchableOpacity 
-          style={{margin: 10}}
-          onPress={() => setOpen(false)}
-          >
-            <Text style={{ fontSize: 24}}>Fechar</Text>
-          </TouchableOpacity>
-        </View>
-      </Modal>
-
-
+      {capturedPhoto && (
+        <Modal animationType="slide" transparent={false} visible={open}>
+          <View
+            style={{
+              flex: 1,
+              justifyContent: 'center',
+              alignItems: 'center',
+              margin: 20,
+            }}>
+            <TouchableOpacity
+              style={{margin: 10}}
+              onPress={() => setOpen(false)}>
+              <Text style={{fontSize: 24}}>Fechar</Text>
+            </TouchableOpacity>
+            <Image
+              resizeMode="contain"
+              style={{width: 350, height: 450, borderRadius: 15}}
+              source={{uri: capturedPhoto}}
+            />
+          </View>
+        </Modal>
+      )}
     </View>
   );
 }
 
 const styles = StyleSheet.create({
-  container:{
-    flex:1,
-    justifyContent: 'center'
+  container: {
+    flex: 1,
+    justifyContent: 'center',
   },
-  preview:{
-    flex:1,
+  preview: {
+    flex: 1,
     justifyContent: 'flex-end',
-    alignItems: 'center'
+    alignItems: 'center',
   },
-  capture:{
+  capture: {
     flex: 0,
     backgroundColor: '#FFF',
     borderRadius: 5,
@@ -83,5 +118,14 @@ const styles = StyleSheet.create({
     paddingHorizontal: 20,
     alignSelf: 'center',
     margin: 20,
+  },
+  camPosition:
+  {
+    backgroundColor: '#FFF',
+    borderRadius: 5,
+    padding: 10,
+    position: 'absolute',
+    right: 25,
+    top: 60
   }
 });
